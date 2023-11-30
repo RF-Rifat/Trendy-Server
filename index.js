e7a0a49e41215983bcc93e58d13e971e4f47f3b8
RF-Rifat <rfrifat6344@gmail.com>
2023-11-30 17:46:14 +0600

    vercel server testing

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 
@@ -258,5 +265,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Trendy Server is Working");
+  console.log("Trendy Server is Running");
 });
